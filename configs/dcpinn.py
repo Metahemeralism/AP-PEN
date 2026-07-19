7388750b6a85047da93f0e0f7fdb6c16be204720
Metahemeralism <eplynch398@gmail.com>
2026-07-19 15:11:54 +0100

    re jig

--- configs/dcpinn.py
+++
@@ -1,4 +0,0 @@
-"""DC-PINN (data-fit + PDE + no-arbitrage constraints) experiment config."""
-from gs_wamol.utils.config import get_dcpinn_config
-
-config = get_dcpinn_config()
--- configs/pinn.py
+++
@@ -1,4 +0,0 @@
-"""PINN (data-fit + PDE residual) experiment config."""
-from gs_wamol.utils.config import get_pinn_config
-
-config = get_pinn_config()
